44c74fa7e77d95350f8e0d2601d5f97f9ebf1d35
Cristhian Saavedra <cristhian@versaplace.com>
2022-02-01 07:14:17 -0300

    Error management in node

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -10,33 +10,50 @@ const pool = new Pool({
 })
 
 const getTemplates = async (req, res) => {
-    const response = await pool.query("SELECT * FROM templates");
-    res.status(200).json(response.rows);
+    try {
+        const response = await pool.query("SELECT * FROM templates");
+        res.status(200).json(response.rows);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const createTemplate = async (req, res) => {
     const { name, author, firstColor, secondColor, type, direction } = req.body;
-    if(await templateExists(name)) {
+    if (await templateExists(name)) {
         res.status(200).send({
             message: 'DUPLICATE'
         });
     } else {
-        const response = await pool.query('INSERT INTO templates VALUES ($1, $2, $3, $4, $5, $6)', [name, author, firstColor, secondColor, direction, type]);
-        res.status(200).send({
-            message: 'OK'
-        });
+        try {
+            const response = await pool.query('INSERT INTO templates VALUES ($1, $2, $3, $4, $5, $6)', [name, author, firstColor, secondColor, direction, type]);
+            res.status(200).send({
+                message: 'OK'
+            });
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
 const getTemplate = async (req, res) => {
     const name = req.params.name;
-    const response = await pool.query(`SELECT * FROM templates WHERE name= $1`, [name]);
-    res.status(200).json(response.rows[0]);
+    try {
+        const response = await pool.query(`SELECT * FROM templates WHERE name= $1`, [name]);
+        res.status(200).json(response.rows[0]);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const templateExists = async (name) => {
-    const response = await pool.query(`SELECT * FROM templates WHERE name= $1`, [name]);
-    return response.rows[0] ? true : false;
+    try {
+        const response = await pool.query(`SELECT * FROM templates WHERE name= $1`, [name]);
+        return response.rows[0] ? true : false;
+    } catch (error) {
+        console.log(error);
+    }
+    return false;
 }
 
 module.exports = {
